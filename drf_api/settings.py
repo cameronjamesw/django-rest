bf3536d28a0e384e44af67837c3d799686d69740
Cam Wheatley <cameron_2000@live.co.uk>
2024-10-10 11:00:55 +0000

    correct error in settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
-if os.exsits('env.py'):
+if os.exsists('env.py'):
     import env
 
 CLOUDINARY_STORAGE = {
