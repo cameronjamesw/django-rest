a32a593454f9057018a9f5421bbeb34ae298505d
Cam Wheatley <cameron_2000@live.co.uk>
2024-10-10 11:01:47 +0000

    fix file path in settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
-if os.exsists('env.py'):
+if os.path.exsists('env.py'):
     import env
 
 CLOUDINARY_STORAGE = {
