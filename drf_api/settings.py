adb9b08d294433287e913da78fb79fa68fb4dbf0
Cam Wheatley <cameron_2000@live.co.uk>
2024-10-10 11:02:29 +0000

    fix spelling error

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
-if os.path.exsists('env.py'):
+if os.path.exists('env.py'):
     import env
 
 CLOUDINARY_STORAGE = {
