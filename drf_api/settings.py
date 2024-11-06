4813881bfeddd742fb3838e80eb4cc1d49e5254e
Cam Wheatley <cameron_2000@live.co.uk>
2024-11-06 16:14:43 +0000

    add further changes for deployment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -63,7 +64,7 @@ DEBUG = 'DEV' in os.environ
 ALLOWED_HOSTS = [
     '8000-cameronjames-djangorest-v0vlcw71cb8.ws-eu116.gitpod.io',
     'cams-api.herokuapp.com',
-    'cams-api-63fe6b3dbe56.herokuapp.com',
+    os.environ.get('ALLOWED_HOST'),
 ]
 
 
@@ -112,9 +113,11 @@ if 'CLIENT_ORIGIN' in os.environ:
         os.environ.get('CLIENT_ORIGIN')
     ]
 else:
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
-    ]
+    if 'CLIENT_ORIGIN_DEV' in os.environ:
+        extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+        CORS_ALLOWED_ORIGIN_REGEXES = [
+            rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+        ]
 
 CORS_ALLOW_CREDENTIALS = True
 
